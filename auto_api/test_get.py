392075d5c3325b8d296e69c4c6dc37c9e9c8db4e
kongxi <kongxi@aiyunxiao.com>
2019-08-15 17:33:56 +0800

    requests库入门-7-json参数请求举例

--- auto_api/test_get.py
+++ auto_api/test_get.py
@@ -1,5 +1,6 @@
-import requests
 import json
+
+import requests
 
 URL = 'https://api.github.com'
 
@@ -9,12 +10,12 @@ def build_uri(endpoint):
 
 
 def better_output(json_str):
-    return json.dumps(json.loads(json_str), indent=4)  # json格式化输出
+    return json.dumps(json.loads(json_str), indent=4)  # 采用json里面提供方法打印出来，格式更好看
 
 
 def request_method():
-    response = requests.get(build_uri('users/haropy'))  # 调用get方法
-    print(better_output(response.text))  # 调用json格式化输出
+    response = requests.get(build_uri('users/Anthonyliu86'))  # 调用get方法，注意用户名这个地方写法，没有图片中冒号
+    print(better_output(response.text))  # 调用json更好格式输出
 
 
 def params_method():
@@ -24,5 +25,14 @@ def params_method():
     print(response.url)
 
 
+def json_method():
+    response = requests.patch(build_uri('user'), auth=('your_account', 'your_password'), json={'email':
+                                                                                                   'abc@msn.com',
+                                                                                               'name': 'Anthony_tester'})
+    print(better_output(response.text))
+    print(response.headers)
+    print(response.url)
+
+
 if __name__ == '__main__':
-    request_method()
+    json_method()
