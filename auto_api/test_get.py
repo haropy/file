f0991c14b2f574e11f3643c9fa4253d347e695b4
kongxi <kongxi@aiyunxiao.com>
2019-08-15 16:31:00 +0800

    requests库入门-4-HTTP请求方法和用requests举例不带参数的请求

---
+++ auto_api/test_get.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+URL = 'https://api.github.com'
+
+
+def build_uri(endpoint):
+    return '/'.join([URL, endpoint])  # 主要作用是拼接接口请求地址
+
+
+def better_output(json_str):
+    return json.dumps(json.loads(json_str), indent=4)  # json格式化输出
+
+
+def request_method():
+    response = requests.get(build_uri('users/haropy'))  # 调用get方法
+    print(better_output(response.text))  # 调用json格式化输出
+
+
+if __name__ == '__main__':
+    request_method()
